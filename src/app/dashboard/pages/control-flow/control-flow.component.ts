f5a1612564dadd2162f62ff2921b78bcccc96a6e
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-03 18:25:08 +0200

    ✨ for directive

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -13,6 +13,9 @@ export default class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
 
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
+
   toggleContent(): void {
     this.showContent.update((value) => !value);
   }
