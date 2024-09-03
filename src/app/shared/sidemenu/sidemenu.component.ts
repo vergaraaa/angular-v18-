207776b1e89c2bf1a6cca2e3721eef6112935445
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-03 17:55:54 +0200

    ✨ menu with new @for syntaxis

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
 })
