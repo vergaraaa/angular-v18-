212f6f1c1ad66d387eb72a66f8a7b7df841a48dc
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-03 18:32:39 +0200

    ✨ transform input attributes

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './title.component.html',
+  template: `<h1 class="text-3xl font-bold mb-5">
+    {{ title }} - {{ withShadow }}
+  </h1>`,
   styles: ``,
 })
-export class TitleComponent {}
+export class TitleComponent {
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+}
