36230dae4bb194755e950177366ab7ad4e19997d
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-04 05:50:46 +0200

    ✨ defer views

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,10 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
   imports: [CommonModule],
-  template: `<h1>Hello world</h1>`,
+  template: `
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      Heavy Loader Slow
+    </section>
+  `,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+  constructor() {
+    const start = Date.now();
+
+    while (Date.now() - start < 3000) {}
+
+    console.log('loaded');
+  }
+
+  @Input({ required: true }) cssClass!: string;
+}
