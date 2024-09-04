1fb142c7e314ed607a82597ce7f6bd028244e343
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-04 06:20:22 +0200

    ✨ hero animation with view transitions

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View Transition 2" />
+
+    <section class="flex justify-end">
+      <img
+        srcset="https://picsum.photos/id/237/200/300"
+        alt="Picsum"
+        width="200"
+        height="300"
+        style="view-transition-name: hero-1"
+      />
+
+      <div
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded-full"
+        style="view-transition-name: hero-2"
+      ></div>
+    </section>
+  `,
+  styles: ``,
+})
+export default class ViewTransitionComponent {}
